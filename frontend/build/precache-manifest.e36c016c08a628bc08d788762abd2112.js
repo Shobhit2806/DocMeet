01364076b1513e84264636f3cfdea418c7fb3cf4
Shobhit2806 <shobhit1256@gmail.com>
2021-05-22 20:06:55 +0530

    patient dashboard build

---
+++ frontend/build/precache-manifest.e36c016c08a628bc08d788762abd2112.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4275e203978383d1c71a8780d5b307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38f3ab632f70811b92b4",
+    "url": "/static/css/2.fdbf91bf.chunk.css"
+  },
+  {
+    "revision": "5ce2648d2065946d649c",
+    "url": "/static/css/main.26a9d406.chunk.css"
+  },
+  {
+    "revision": "38f3ab632f70811b92b4",
+    "url": "/static/js/2.ade61117.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/static/js/2.ade61117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce2648d2065946d649c",
+    "url": "/static/js/main.404329b5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4bb231bcd1e74af065a6c12f0604eb35",
+    "url": "/static/media/bookavisit.4bb231bc.svg"
+  },
+  {
+    "revision": "3d623091bad91fc0d0af15c3b6300cc7",
+    "url": "/static/media/calendar.3d623091.svg"
+  },
+  {
+    "revision": "362da4639b346ebcd89b7c46ddd9b793",
+    "url": "/static/media/chat.362da463.svg"
+  },
+  {
+    "revision": "62772ee860afaa1922babff211d01ccb",
+    "url": "/static/media/dietician.62772ee8.jpg"
+  },
+  {
+    "revision": "800aae1140827683626cfbecf5ddd349",
+    "url": "/static/media/findadoctor.800aae11.svg"
+  },
+  {
+    "revision": "3d36874b10ce6893beb601298576272f",
+    "url": "/static/media/imageslider1.3d36874b.svg"
+  },
+  {
+    "revision": "74996533414bbc7096df727bb566571d",
+    "url": "/static/media/img1.74996533.svg"
+  },
+  {
+    "revision": "093f1861fc92b3a0ba497ece177b27e6",
+    "url": "/static/media/login1.093f1861.gif"
+  },
+  {
+    "revision": "05d303cd31bb3d3aab771cb858f64d2e",
+    "url": "/static/media/logo.05d303cd.png"
+  },
+  {
+    "revision": "7e5a8fd927a12c303adae4d27c81a448",
+    "url": "/static/media/medical-history.7e5a8fd9.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ef0dd2f4ac06617e6c4db93900725c77",
+    "url": "/static/media/viewprofile.ef0dd2f4.svg"
+  }
+]);
